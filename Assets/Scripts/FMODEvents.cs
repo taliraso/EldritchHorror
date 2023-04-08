61b1d195f5ddcde0621322354a686cc5621daced
traso <talia.raso@gmail.com>
2023-04-08 17:23:21 +1000

    [TR] Fixed balcony atmos event bug.

--- Assets/Scripts/FMODEvents.cs
+++ Assets/Scripts/FMODEvents.cs
@@ -10,23 +10,6 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference speechBubbleAudio { get; private set; }
     [field: SerializeField] public EventReference radioTextAudio { get; private set; }
 
-    [field: Header("Dog SFX")]
-    [field: SerializeField] public EventReference audiodogjump { get; private set; }
-    [field: SerializeField] public EventReference audiodogbark { get; private set; }
-    [field: SerializeField] public EventReference audiodoghowl { get; private set; }
-    [field: SerializeField] public EventReference audiodogslurp { get; private set; }
-    [field: SerializeField] public EventReference audiodogwhine { get; private set; }
-    [field: SerializeField] public EventReference audiodogmunch { get; private set; }
-
-    [field: Header("Interactables SFX")]
-    [field: SerializeField] public EventReference audiodooruse { get; private set; }
-    [field: SerializeField] public EventReference audioplayeroutofbed { get; private set; }
-    [field: SerializeField] public EventReference audioaura { get; private set; }
-    [field: SerializeField] public EventReference audioradioloop { get; private set; }
-    [field: SerializeField] public EventReference audioscream1 { get; private set; }
-    [field: SerializeField] public EventReference audioscream2 { get; private set; }
-    [field: SerializeField] public EventReference audioplayerthrowball { get; private set; }
-
     [field: Header("Music")]
     [field: SerializeField] public EventReference neutralTheme { get; private set; }
     [field: SerializeField] public EventReference happyTheme { get; private set; }
